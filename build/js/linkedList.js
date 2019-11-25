6c3e47d3f8009ad4d7f747b03efb86598668f67d
shotasan <wondermoai@gmail.com>
2019-11-25 23:31:38 +0900

    リスト表示部分

---
+++ build/js/linkedList.js
@@ -0,0 +1,42 @@
+"use strict";
+var StationList = /** @class */ (function () {
+    function StationList() {
+        this.name = '';
+        this.next = -1;
+    }
+    return StationList;
+}());
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
+        this.list = new Array(10);
+        this.top = null;
+    }
+    LinkedList.prototype.initStationList = function () {
+        for (var i = 0; i < this.list.length; i++) {
+            this.list[i] = new StationList;
+        }
+        this.list[0].name = "新大阪";
+        this.list[0].next = -1;
+        this.list[1].name = "名古屋";
+        this.list[1].next = 3;
+        this.list[2].name = "東京";
+        this.list[2].next = 4;
+        this.list[3].name = "京都";
+        this.list[3].next = 0;
+        this.list[4].name = "新横浜";
+        this.list[4].next = 1;
+        this.top = 2;
+    };
+    LinkedList.prototype.printStationList = function () {
+        var idx = this.top;
+        while (idx != -1) {
+            console.log(this.list[idx].name + " \u2192 ");
+            idx = this.list[idx].next;
+        }
+    };
+    return LinkedList;
+}());
+var list = new LinkedList();
+list.initStationList();
+list.printStationList();
+//# sourceMappingURL=linkedList.js.map
---
+++ linkedList.ts
@@ -0,0 +1,45 @@
+class StationList {
+  name: string;
+  next: number;
+
+  constructor() {
+    this.name = '';
+    this.next = -1;
+  }
+}
+
+class LinkedList {
+  list: any;
+  top: any;
+
+  constructor() {
+    this.list = new Array(10);
+    this.top = null;
+  }
+
+  initStationList() {
+    for (let i: number = 0; i < this.list.length; i++) {
+      this.list[i] = new StationList;
+    }
+    this.list[0].name = "新大阪"; this.list[0].next = -1;
+    this.list[1].name = "名古屋"; this.list[1].next = 3;
+    this.list[2].name = "東京"; this.list[2].next = 4;
+    this.list[3].name = "京都"; this.list[3].next = 0;
+    this.list[4].name = "新横浜"; this.list[4].next = 1;
+
+    this.top = 2;
+  }
+
+  printStationList() {
+    let idx: number = this.top;
+
+    while (idx != -1) {
+      console.log(`${this.list[idx].name} → `);
+      idx = this.list[idx].next;
+    }
+  }
+}
+
+let list = new LinkedList();
+list.initStationList();
+list.printStationList();
